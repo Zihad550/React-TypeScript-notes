327e5b188d5546f6e37b3054e64ab542cb9da096
Zihad550 <hussainmuzahid83@gmail.com>
2022-02-01 16:08:29 +0600

    note-7 updated

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-# Note-6
+# Note-7
 
-## use Type on useState hooks
+## Type Assertion
 
-- Most of the time we don't have to explicitly set the use state value type. TypeScript automatically sets the value type. **Loggedin.tsx** for example.
-
-- To use type on useState hook see user.tsx
-- You only use when the initial value type is different compared with future value type.
+- You can use it to tell typescript that the object is not null
--- src/components/User.tsx
+++ src/components/User.tsx
@@ -5,22 +5,20 @@ type AuthUser = {
   email: string;
 };
 const User = () => {
-  const [user, setUser] = useState<null | AuthUser>({ email: "", name: "" });
+  // for type assertion as keyword is used
+  const [user, setUser] = useState<AuthUser>({} as AuthUser);
   const handleLogin = () => {
     setUser({
       name: "Jehad",
       email: "jehad@gmail.com",
     });
   };
-  const handleLogOut = () => {
-    setUser(null);
-  };
+
   return (
     <div>
       <button onClick={handleLogin}>Log In</button>
-      <button onClick={handleLogOut}>Log Out</button>
-      <p>User name is {user?.name}</p>
-      <p>User email is {user?.email}</p>
+      <p>User name is {user.name}</p>
+      <p>User email is {user.email}</p>
     </div>
   );
 };
